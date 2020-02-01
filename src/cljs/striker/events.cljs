064c6d0ccf915c5f21dc104f4fe76b59446ec90e
Matej Kupciha <kupciham@hotmail.com>
2020-02-01 21:30:11 +0100

    Update striker.events

--- src/cljs/striker/events.cljs
+++ src/cljs/striker/events.cljs
@@ -1,10 +1,21 @@
 (ns striker.events
   (:require
-   [re-frame.core :as re-frame]
-   [striker.db :as db]
-   ))
+    [clojure.string :as string]
+    [re-frame.core :as re-frame]
+    [striker.db :as db]))
 
 (re-frame/reg-event-db
- ::initialize-db
- (fn [_ _]
-   db/default-db))
+  ::initialize-db
+  (fn [_ _]
+    db/default-db))
+
+(re-frame/reg-event-db
+  ::set-active-panel
+  (fn [db [_ active-panel]]
+    (assoc db :active-panel active-panel)))
+
+(re-frame/reg-event-db
+  ::filter-search-results
+  (fn [db [_ query]]
+    (assoc db :search-results (filter #(string/includes? (string/lower-case (:name %)) (string/lower-case query))
+                                      (:search-results db/default-db)))))
