e6eefde0898fd0743f96d7c56dbb75499cd453c9
Matej Kupciha <kupciham@hotmail.com>
2020-02-01 22:13:59 +0100

    Update striker.subs

--- src/cljs/striker/subs.cljs
+++ src/cljs/striker/subs.cljs
@@ -1,8 +1,18 @@
 (ns striker.subs
   (:require
-   [re-frame.core :as re-frame]))
+    [re-frame.core :as re-frame]))
 
 (re-frame/reg-sub
- ::name
- (fn [db]
-   (:name db)))
+  ::active-panel
+  (fn [db _]
+    (:active-panel db)))
+
+(re-frame/reg-sub
+  ::search-results
+  (fn [db _]
+    (:search-results db)))
+
+(re-frame/reg-sub
+  ::hotel
+  (fn [db _]
+    (:hotel db)))
