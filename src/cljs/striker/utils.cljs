b466c99d0a0b2366289a1fb3abfcde6fe9bad1ba
Matej Kupciha <kupciham@hotmail.com>
2020-02-10 09:43:35 +0100

    Replace fixtures call with real backend

---
+++ src/cljs/striker/utils.cljs
@@ -0,0 +1,13 @@
+(ns striker.utils)
+
+(defn unwrap [input keyword-set]
+  (cond
+    (map? input) (reduce-kv (fn [map key value]
+                              (if (contains? keyword-set key)
+                                  (merge map (unwrap value keyword-set))
+                                  (assoc map key (unwrap value keyword-set))))
+                            {} input)
+    (vector? input) (reduce (fn [vector value]
+                              (conj vector (unwrap value keyword-set)))
+                            [] input)
+    :else input))
